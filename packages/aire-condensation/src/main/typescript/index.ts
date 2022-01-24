98c909f74bb62619074117187897a1e718cad544
Josiah Haswell <josiah.haswell@gmail.com>
2022-01-24 15:18:08 -0700

    Adding condensation

---
+++ packages/aire-condensation/src/main/typescript/index.ts
@@ -0,0 +1,4 @@
+export { Region, Address, Pointer } from "./types";
+export { Condensation, Context } from "./condensation";
+export { Remotable, Receive } from "./remotable";
+export { RootElement, Property } from "./root-element";
